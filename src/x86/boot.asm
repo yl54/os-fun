fa7838f74c7b4c52f9b26dbae62fe2150adbf0fd
Randy Li <randyl@digitalchemy.us>
2018-07-08 14:42:03 -0700

    src/x86/: copy paste exact

--- src/x86/boot.asm
+++ src/x86/boot.asm
@@ -1,13 +1,8 @@
 global start
 
-; the .text section is the default location for executable code.
 section .text
-
-; specify that these are 32 bit instructions. in this case, the
-;   cpu is in protected mode when GRUB starts the kernel.
 bits 32
-
 start:
-    ; print 'ok' to the screen
+    ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
--- src/x86/multiboot_header.asm
+++ src/x86/multiboot_header.asm
@@ -1,17 +1,15 @@
 section .multiboot_header
 header_start:
-    dd 0xe85250d6                 ; magic number
-    dd 0                          ; architecture 0 (protected mode i386)
-    dd header_end - header_start  ; header length
-
+    dd 0xe85250d6                ; magic number (multiboot 2)
+    dd 0                         ; architecture 0 (protected mode i386)
+    dd header_end - header_start ; header length
     ; checksum
-    dd 0x10000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
-    ; optional multiboot tags if necessary
+    ; insert optional multiboot tags here
 
     ; required end tag
     dw 0    ; type
     dw 0    ; flags
     dd 8    ; size
 header_end:
-
